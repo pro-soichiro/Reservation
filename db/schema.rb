bae7b07d089b7c91fa06ecc58c3a85602b60e64c
pro-soichiro <dr.1987626@gmail.com>
2022-04-04 07:45:01 +0000

    Roomモデルとseedファイルの更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_04_040027) do
+ActiveRecord::Schema.define(version: 2022_04_04_072638) do
 
   create_table "entries", force: :cascade do |t|
     t.string "user_name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_04_04_040027) do
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "terms_of_use"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do |n|
+  Room.create!(
+    name:"部屋名#{n}",
+    place:"会議室#{n}",
+    number: n,
+    terms_of_use: "#{n}人以上で使用しないこと",
+    created_at: "2022-04-04 14:00:00",
+    updated_at: "2022-04-04 14:00:00"
+  )
+end
