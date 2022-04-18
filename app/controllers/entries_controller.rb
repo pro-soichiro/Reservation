4278d01a4889b154bf23e2ec7c75e59bfbb07047
pro-soichiro <dr.1987626@gmail.com>
2022-04-18 10:03:27 +0000

    Entriesコントローラーの更新とダイジェスト認証機能の追加

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,38 @@
 class EntriesController < ApplicationController
+  before_action :set_entry, only: [:destroy]
+
   def new
+    @entry = Entry.new
   end
 
   def create
+    @entry = Entry.new(entry_params)
+
+    respond_to do |format|
+      if @entry.save
+        format.html { redirect_to @entry,notice: "Entry was successfully created." }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def destroy
   end
 
   def index
+    @entries = Entry.all
   end
 
   def confirm
   end
+
+  private
+    def set_entry
+      @entry = Entry.find(params[:id])
+    end
+
+    def entry_params
+      params.require(:entry).permit(:use_name,:user_email,:reserved_date,:usage_time,:room_id,:people)
+    end
 end
