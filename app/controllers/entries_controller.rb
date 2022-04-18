d42c0cf3d4be9061c848a7a59bb90407e3a9b0d8
pro-soichiro <dr.1987626@gmail.com>
2022-04-18 02:56:24 +0000

    Entryのルーティングの設定とコントローラーの作成

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,16 @@
+class EntriesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def confirm
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  root to: 'top#index'
   resources :rooms
+  resources :entries, except: [:show,:update,:edit], path: :rentals do
+    post :confirm, on: :collection
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
