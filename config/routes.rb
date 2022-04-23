00bf37aad814b83cc6801f3fa6633f3508df108b
pro-soichiro <dr.1987626@gmail.com>
2022-04-23 03:31:04 +0000

    ルーティング変更による宛先URIの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :users
   root to: 'top#index'
-  resources :rooms
-  resources :entries, except: [:show,:update,:edit], path: :rentals do
-    post :confirm, on: :collection
-    post :confirm_back, on: :collection
+  resources :rooms, shallow: true do
+    resources :entries, except: [:show,:update,:edit], path: :rentals do
+      post :confirm, on: :collection
+      post :confirm_back, on: :collection
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
