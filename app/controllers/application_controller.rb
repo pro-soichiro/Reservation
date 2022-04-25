e1b381300d903754eb3c0972f38ac16066b61374
pro-soichiro <dr.1987626@gmail.com>
2022-04-25 12:01:27 +0000

    HTTP認証の取り除き

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,7 @@
 class ApplicationController < ActionController::Base
-  USERS = {"admin_user" => "admin_password"}
-  before_action :authenticate
   before_action :set_locale
 
   private
-    def authenticate
-      authenticate_or_request_with_http_digest do |username|
-        USERS[username]
-      end
-    end
 
     def set_locale
       I18n.locale = params[:locale] || I18n.default_locale
