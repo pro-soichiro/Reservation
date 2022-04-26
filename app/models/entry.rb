4f2294c03a102cf380f3d202a0bc74ccfd08bb94
pro-soichiro <dr.1987626@gmail.com>
2022-04-26 03:10:28 +0000

    TODOの追加

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,9 +1,10 @@
 class Entry < ApplicationRecord
   belongs_to :room
+  # TODO: 予約エントリのuser_idだけでユーザーの情報とひも付けて、予約エントリのユーザー名やメールアドレスを持たなくても良いよう変更
 
   scope :least_entries, ->(date) { where("reserved_date BETWEEN ? AND ?",date.to_date - 7.days,date.to_date + 7.days ) }
 
   validates :user_name,:user_email,:reserved_date,:usage_time,:people,:room_id,presence: true
   validates :user_email,uniqueness: true
-  
+
 end
