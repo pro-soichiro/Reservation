06735fa0cb20a13d2ce4ed94d6125f4f8cc19fd2
pro-soichiro <dr.1987626@gmail.com>
2022-05-02 13:10:48 +0000

    auths_controller_test 解答

--- test/controllers/auths_controller_test.rb
+++ test/controllers/auths_controller_test.rb
@@ -2,12 +2,20 @@ require 'test_helper'
 
 class AuthsControllerTest < ActionDispatch::IntegrationTest
   test "ログイン画面呼び出し" do
+    get new_auths_url
+    assert_response :success
   end
   test "ログイン処理" do
+    post auths_url, params: {auth: { email: "taro@sample.com", password: "tarotaro123" }}
+    assert_response :redirect
   end
   test "ログイン失敗処理" do
+    post auths_url, params: {auth: { email: "taro@sample.com", password: "taro123" }}
+    assert_response :redirect
   end
   test "ログアウト処理" do
+    delete auths_url
+    assert_equal nil,session[:user_id]
   end
 
 end
