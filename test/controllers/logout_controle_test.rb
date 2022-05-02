cc94935021ba1ddc6e6f4490bc9f57cdbc0f17f1
pro-soichiro <dr.1987626@gmail.com>
2022-05-02 13:29:40 +0000

    logout_controle_test.rb 解答

--- test/controllers/logout_controle_test.rb
+++ test/controllers/logout_controle_test.rb
@@ -4,23 +4,43 @@ class LogoutControlTest < ActionDispatch::IntegrationTest
 
   # 未ログイン状態でのログイン制御
   test "トップページの接続" do
+    get root_url(locale: "ja")
+    assert_redirected_to new_auths_url(locale: "ja")
   end
   test "会議室一覧の接続" do
+    get rooms_url(locale: "ja")
+    assert_redirected_to new_auths_url(locale: "ja")
   end
   test "会議室登録の接続" do
+    get new_room_url(locale: "ja")
+    assert_redirected_to new_auths_url(locale: "ja")
   end
   test "会議室編集の接続" do
+    get edit_room_url(rooms(:one))
+    assert_redirected_to new_auths_url(locale: "ja")
   end
   test "会議室削除の接続" do
+    delete room_url(rooms(:one))
+    assert_redirected_to new_auths_url(locale: "ja")
   end
   test "予約登録の接続" do
+    get new_room_entry_url(rooms(:one))
+    assert_redirected_to new_auths_url(locale: "ja")
   end
   test "予約取り消しの接続" do
+    delete entry_url(entries(:one))
+    assert_redirected_to new_auths_url(locale: "ja")
   end
   test "ユーザー登録処理（サインアップ）" do
+    get new_user_url(locale: "ja")
+    assert_response :success
   end
   test "ユーザー編集の接続" do
+    get edit_user_url(users(:one))
+    assert_redirected_to new_auths_url(locale: "ja")
   end
   test "ユーザー削除の接続" do
+    delete user_url(users(:one))
+    assert_redirected_to new_auths_url(locale: "ja")
   end
 end
